62050e6084186ae5714b1d7c19aba2e65b830f70
Janardhanan Shanmugavel Duraipandi <janardhanan.sd@us.ibm.com>
2020-08-07 19:10:12 +0000

    updated code with message

--- api/controllers/hello_world.js
+++ api/controllers/hello_world.js
@@ -37,8 +37,8 @@ module.exports = {
  */
 function hello(req, res) {
   // variables defined in the Swagger document can be referenced using req.swagger.params.{parameter_name}
-  var name = req.swagger.params.name.value || 'stranger';
-  var helloname = util.format('Hello, %s!', name);
+  var name = req.swagger.params.name.value || 'Jana';
+  var helloname = util.format('Hello Jana, %s!', name);
 
   // this sends back a JSON response which is a single string
   res.json(helloname);
